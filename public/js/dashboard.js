88fcc626fe9408538409a98e19de74338c587616
LlayN <h.yengo@outlook.fr>
2024-10-10 18:36:40 +0200

    Création de la page d'accueil et du template de base (navbar, menu, footer..)

---
+++ public/js/dashboard.js
@@ -0,0 +1,34 @@
+let switchState = false;
+
+const clickOnSwitch = document.querySelector("#Schedules_closedPark");
+
+if (clickOnSwitch.checked) {
+    disableInput();
+    switchState = true;
+}
+
+clickOnSwitch.addEventListener("click", () => {
+    if (switchState == false) {
+        switchState = true;
+        disableInput();
+    } else {
+        switchState = false;
+        enableInput();
+    }
+});
+
+function disableInput() {
+    document
+        .querySelector("#Schedules_closesAt")
+        .setAttribute("disabled", "disabled");
+    document
+        .querySelector("#Schedules_opensAt")
+        .setAttribute("disabled", "disabled");
+    document.querySelector("#Schedules_closesAt").value = "";
+    document.querySelector("#Schedules_opensAt").value = "";
+}
+
+function enableInput() {
+    document.querySelector("#Schedules_closesAt").removeAttribute("disabled");
+    document.querySelector("#Schedules_opensAt").removeAttribute("disabled");
+}
