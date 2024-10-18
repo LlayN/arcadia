ab1bd67145529aaded2c06bada0f5af0ae217897
LlayN <h.yengo@outlook.fr>
2024-10-19 00:25:27 +0200

    Modification des boutons de soumission de formulaire pour éviter le spam

--- src/Form/TestimonialsType.php
+++ src/Form/TestimonialsType.php
@@ -2,31 +2,29 @@
 
 namespace App\Form;
 
-
+use App\Entity\Testimonials;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ContactType extends AbstractType
+class TestimonialsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('mail', EmailType::class)
-            ->add('title', TextType::class)
-            ->add('description', TextareaType::class)
-            ->add('submit', SubmitType::class, ['label' => 'Envoyer ma demande', 'attr' => ['class' => 'btn-submit back-primary p-3 px-5 rounded-0 text-white m-0 w-100 border-0']])
+            ->add('username', TextType::class, ['label' => 'Nom d\'utilisateur'])
+            ->add('opinion', TextareaType::class, ['label' => 'Votre avis'])
+            ->add('submit', SubmitType::class, ['label' => 'Envoyer mon avis', 'attr' => ['class' => 'btn-submit back-primary p-3 px-5 rounded-0 text-white m-0 w-100 border-0']])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => Testimonials::class,
         ]);
     }
 }
