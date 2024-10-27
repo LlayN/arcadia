e75c58db4cac692d4fca852a054d404468c14a65
LlayN <h.yengo@outlook.fr>
2024-10-27 13:52:42 +0100

    Modification du formulaire d'avis

--- src/Form/TestimonialsType.php
+++ src/Form/TestimonialsType.php
@@ -15,6 +15,7 @@ class TestimonialsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->setMethod('POST')
             ->add('username', TextType::class, ['label' => 'Nom d\'utilisateur'])
             ->add('opinion', TextareaType::class, ['label' => 'Votre avis'])
             ->add('submit', SubmitType::class, ['label' => 'Envoyer mon avis', 'attr' => ['class' => 'btn-submit back-primary p-3 px-5 rounded-0 text-white m-0 w-100 border-0']])
