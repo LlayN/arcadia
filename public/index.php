128cedb9cd2c8ca992e8b56dcb2aa4e5aa7d5b0a
LlayN <h.yengo@outlook.fr>
2024-10-27 11:06:31 +0100

    Modification de index.php

--- public/index.php
+++ public/index.php
@@ -5,21 +5,6 @@
 use App\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
-
-if (getenv('JAWSDB_URL') !== false) {
-    $dbparts = parse_url(getenv('JAWSDB_URL'));
-
-    $hostname = $dbparts['host'];
-    $username = $dbparts['user'];
-    $password = $dbparts['pass'];
-    $database = ltrim($dbparts['path'], '/');
-} else {
-    $hostname = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'arcadia-ecf';
-}
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
