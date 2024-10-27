f5b3ee7f4e5f1d9a43a2fb74f7b1ee7dbecc3508
LlayN <h.yengo@outlook.fr>
2024-10-27 14:07:28 +0100

    Modification du formulaire d'avis

--- src/Form/TestimonialsType.php
+++ src/Form/TestimonialsType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Csrf\CsrfToken;
 
 class TestimonialsType extends AbstractType
 {
@@ -19,6 +20,7 @@ class TestimonialsType extends AbstractType
             ->add('username', TextType::class, ['label' => 'Nom d\'utilisateur'])
             ->add('opinion', TextareaType::class, ['label' => 'Votre avis'])
             ->add('submit', SubmitType::class, ['label' => 'Envoyer mon avis', 'attr' => ['class' => 'btn-submit back-primary p-3 px-5 rounded-0 text-white m-0 w-100 border-0']])
+            ->add('', CsrfToken::class, ['csrf_protection' => true])
         ;
     }
 
