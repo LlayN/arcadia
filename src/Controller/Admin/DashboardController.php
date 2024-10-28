ecd16b4a870c306d493f4c18608f8ec1ff178dde
LlayN <h.yengo@outlook.fr>
2024-10-28 20:15:08 +0100

    Modification URL relative pour le dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -66,6 +66,7 @@ class DashboardController extends AbstractDashboardController
     {
         return Dashboard::new()
             ->setTitle('<img src="/uploads/admin/logo/logo-black.svg" alt="" class="logo">')
+            ->generateRelativeUrls()
             ->disableDarkMode();
     }
 
