86dce3a338accc2b4d5df97049c816a89f6a1822
LlayN <h.yengo@outlook.fr>
2024-11-24 23:05:11 +0100

    Modif HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,27 +21,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(EntityManagerInterface $entityManagerInterface, Request $request, DocumentManager $dm): Response
     {
-        // Vérifier si le ID session n'existe pas déjà dans la BDD
 
-
-        $repository = $dm->getRepository(Visitor::class);
-
-
-
-
-        $visitor = new Visitor();
-        $visitor->setSessionId($request->getSession()->getId());
-        $visitor->setIpAddress($request->getClientIp());
-        $visitor->setVisitedAt(DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s')));
-        $visitor->setLanguage($request->getPreferredLanguage());
-        $visitor->setUserAgent($request->headers->get('User-Agent'));
-
-        $dm->persist($visitor);
-        $dm->flush();
-
-
-
-        $repository = $dm->getRepository(Visitor::class);
+        /* $repository = $dm->getRepository(Visitor::class);
 
         $visitor = $repository->findOneBy(['session_id' => $request->getSession()->getId()]);
 
@@ -55,7 +36,7 @@ class HomeController extends AbstractController
 
             $dm->persist($visitor);
             $dm->flush();
-        }
+        } */
 
 
         $testimonial = new Testimonials();
