631affecfd12bd498576c17ca8ac67960f780dde
LlayN <h.yengo@outlook.fr>
2024-11-24 10:53:39 +0100

    Création d'un analytic traffic, transmis à MongoDB et afficher sur dashboard Admin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,44 @@
 
 namespace App\Controller;
 
+use App\Document\Visitor;
 use App\Entity\Schedules;
 use App\Entity\Services;
 use App\Entity\Testimonials;
 use App\Form\TestimonialsType;
+use DateTime;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $entityManagerInterface, Request $request): Response
+    public function index(EntityManagerInterface $entityManagerInterface, Request $request, DocumentManager $dm): Response
     {
+        // Vérifier si le ID session n'existe pas déjà dans la BDD
+
+
+        $repository = $dm->getRepository(Visitor::class);
+
+        $visitor = $repository->findOneBy(['session_id' => $request->getSession()->getId()]);
+
+        if (!isset($visitor)) {
+            $visitor = new Visitor();
+            $visitor->setSessionId($request->getSession()->getId());
+            $visitor->setIpAddress($request->getClientIp());
+            $visitor->setVisitedAt(DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s')));
+            $visitor->setLanguage($request->getPreferredLanguage());
+            $visitor->setUserAgent($request->headers->get('User-Agent'));
+
+            $dm->persist($visitor);
+            $dm->flush();
+        }
+
 
         $testimonial = new Testimonials();
         $form = $this->createForm(TestimonialsType::class, $testimonial);
